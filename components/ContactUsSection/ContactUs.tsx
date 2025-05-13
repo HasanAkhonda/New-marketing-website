e161051c89828d9a836c71bbafbb123163416b9f
hasanakhonda <mdmahmudhasanakhonda@gmail.com>
2025-05-13 14:31:55 +0600

    medium device enabled

--- components/ContactUsSection/ContactUs.tsx
+++ components/ContactUsSection/ContactUs.tsx
@@ -9,7 +9,7 @@ const ContactUs = () => {
         background: `linear-gradient(130.48deg, rgba(237, 227, 254, 0.6) 0.52%, rgba(255, 255, 255, 0.6) 51.91%, rgba(237, 227, 254, 0.6) 100.01%)`,
       }}
     >
-      <div className="bg-white rounded shadow-md max-w-5xl mx-5 md:mx-auto my-8 md:my-24 px-4 py-6 md:py-12 md:px-16">
+      <div className="bg-white rounded shadow-md max-w-4xl mx-5 lg:mx-auto my-8 md:my-24 px-4 py-6 md:py-12 md:px-16">
         <h2 className="font-bold text-center text-[32px] sm:text-[40px] md:text-[48px] mb-12">
           Contact Us Today
         </h2>
